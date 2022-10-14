348f9e60229e923faff981cb66019aaeb9238eaa
zeler <gamouh.mehdi@gmail.com>
2022-10-14 10:46:34 +0200

    date display function

--- src/components/main/calendrier/Calendrier.js
+++ src/components/main/calendrier/Calendrier.js
@@ -3,32 +3,52 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {FaArrowLeft  } from "react-icons/fa"
 import { FaArrowRight } from "react-icons/fa"
+import {useState} from 'react';
+import { getValue } from "@testing-library/user-event/dist/utils";
+
+
 
 function Calendrier(props) {
-    let date = new Date 
-let week = []
-let tab_jour=new Array("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche");
+    let date = new Date ()
+    let weekDays = []
 
-for (let i = 1; i <= 7; i++) {
-  let first = date.getDate() - date.getDay() + i 
-  let day = new Date(date.setDate(first)).toISOString().slice(0, 10)
-  week.push(day)
- 
-}
+    for (let i = 1; i <= 7; i++) {
+    let first = date.getDate() - date.getDay() + i 
+    let day = new Date(date.setDate(first)).toLocaleString('fr-FR', { month: 'long', day: 'numeric', weekday: 'long' })
+    weekDays.push(day.split(' '))
+    };
+    const [isVisible, setIsVisible] = useState();
 
+    const handleClick = event => {
+      
+      setIsVisible(current => !current);
+    };
 
-
+      
   return (
-   
     <section>
- 
       <h4 className="mainTextColor mt-5">Planning</h4> 
       <div class="secondaryBgColor mt-3 py-5" id="planningContainer">
         <Row>
             <Col xs="12" md="6">
                 <Row>
+                    {weekDays.map((weekDay) => {
+                            return (
+                                <Col xs="4" className="d-flex justify-content-center align-items-center ">
+                                    <div className=" p-5 my-2 mainBgColor text-center border border-dark fw-bold position-relative  "  onClick={handleClick}>
+                                        { weekDay[0][0].toUpperCase() }
+                                        <div className="position-absolute top-0 start-0 p-2 small">{ weekDay[1]}</div>
+                                        <div className="position-absolute top-0 end-0 p-2 m-2"></div>
+                                        
+                                    </div>
+                                </Col>
 
-                        <Col xs="4" className="d-flex justify-content-center align-items-center ">
+                            );
+                    })}
+                    
+               
+
+                        {/* <Col xs="4" className="d-flex justify-content-center align-items-center ">
     
 
                                <div className=" p-5 mainBgColor text-center border border-dark fw-bold position-relative ">L  
@@ -96,7 +116,7 @@ for (let i = 1; i <= 7; i++) {
                                 <div className="position-absolute top-0 end-0 p-2 m-2"></div>
                            
                             </div>
-                        </Col>
+                        </Col> */}
 
                         <Col xs="4" className="d-flex justify-content-center align-items-center">
                             <span className="me-1  p-1 mainBgColor border border-dark">{<FaArrowLeft />}</span>
@@ -105,6 +125,8 @@ for (let i = 1; i <= 7; i++) {
                 </Row>
             </Col>
             <Col xs="12" md="6">
+            <p style={{visibility: isVisible ? 'visible' : 'hidden'}}>{weekDays[1] } </p>
+       
                 <div class="row my-2">
                     <div class=" col-md-8 p-3 calendar  text-center fw-bold  ">
                         Club Adulte
